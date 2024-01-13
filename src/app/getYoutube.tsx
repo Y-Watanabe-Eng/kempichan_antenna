ee7638e996f9c8a95ca6ff1a485bb54b9d7776ce
Y.Watanabe <tofu.mental@gmail.com>
2024-01-13 23:35:24 +0900

    up

---
+++ src/app/getYoutube.tsx
@@ -0,0 +1,148 @@
+"use server"
+
+export default async function getYoutube() {
+
+  const apiKey = process.env.YOUTUBE_API_KEY
+  const channelId = "UCUBvFfyuBrATTPxnAZ4OsCQ"
+
+//チャンネル情報の取得
+  async function getChannel() {
+
+    const channelRes = await fetch(
+      "https://www.googleapis.com/youtube/v3/channels?part=" +
+        "contentDetails" +
+        "&id=" +
+        channelId + 
+        "&key=" +
+        apiKey,
+      { cache: 'no-store', }
+    )
+      
+    const channelData = await channelRes.json()
+
+    return channelData;
+
+  }
+
+  const channelData = await getChannel()
+
+  const uploadsId = channelData.items[0].contentDetails.relatedPlaylists.uploads
+
+
+//プレイリスト情報の取得
+  async function getPlaylist() {
+
+    const playlistRes = await fetch(
+      "https://www.googleapis.com/youtube/v3/playlistItems?part=" +
+        "contentDetails" +
+        "&playlistId=" +
+        uploadsId +
+        "&maxResults=50" +
+        "&key=" +
+        apiKey,
+      { cache: 'no-store', }
+    )
+      
+    const playlistData = await playlistRes.json()
+
+    return playlistData;
+
+  }
+
+  const playlistData = await getPlaylist()
+
+  const videoIdArray: string[] = []
+
+  for (let i = 0; i < playlistData.items.length; i++) {
+    videoIdArray.push(playlistData.items[i].contentDetails.videoId)
+  }
+
+
+  console.log(videoIdArray)
+
+
+//動画データの取得
+  async function getVideo() {
+
+    const videoRes = await fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=" +
+        "snippet,statistics" +
+        "&id=" +
+        videoIdArray +
+        "&key=" +
+        apiKey,
+      { cache: 'no-store', }
+    )
+      
+    const videoData = await videoRes.json()
+
+    return videoData.items;
+
+  }
+
+  let videoData = await getVideo()
+
+//playlistData = プレイリスト情報のJSONファイル（動画ID取得用）
+//videoData = 動画情報のJSONファイル
+//videoIdArray = 動画IDを格納した配列
+
+
+//プレイリスト情報の次ページを取得
+  let pageToken = playlistData.nextPageToken
+
+  async function getPlaylistNext() {
+
+    while (pageToken !== undefined) {
+
+      const nextPlaylistRes = await fetch(
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=" +
+          "contentDetails" +
+          "&playlistId=" +
+          uploadsId +
+          "&maxResults=50" +
+          "&pageToken=" +
+          pageToken +
+          "&key=" +
+          apiKey,
+      { cache: 'no-store', }
+    )
+
+      const nextPlaylistData = await nextPlaylistRes.json()
+
+      let nextVideoIdArray: string[] = []
+
+      for (let j = 0; j < nextPlaylistData.items.length; j++) {
+        nextVideoIdArray.push(nextPlaylistData.items[j].contentDetails.videoId)
+      }
+
+      if (nextPlaylistData.nextPageToken !== undefined) {
+        pageToken = nextPlaylistData.nextPageToken
+      } else {
+        pageToken = undefined
+      }
+
+      const nextVideoRes = await fetch(
+        "https://www.googleapis.com/youtube/v3/videos?part=" +
+          "snippet,statistics" +
+          "&id=" +
+          nextVideoIdArray +
+          "&key=" +
+          apiKey,
+      { cache: 'no-store', }
+    )
+          
+      let nextVideoData = await nextVideoRes.json()
+
+      videoData = videoData.concat(nextVideoData.items)
+
+    }
+
+    return videoData
+
+  }
+
+  await getPlaylistNext()
+
+  return videoData
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,261 @@
+"use client"
+
+import './globals.css'
+import { useEffect, useState } from 'react'
+import getYoutube from './getYoutube'
 import Image from 'next/image'
+import Pagination from '@mui/material/Pagination'
+import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
 
-export default function Home() {
+
+interface items {
+  id: string
+  snippet: {
+    title: string
+    publishedAt: string
+    description: string
+    thumbnails: {
+      high: {
+        url: string
+      }
+    }
+  }
+  statistics: {
+    viewCount: string
+    likeCount: string
+    commentCount: string 
+  }
+}
+
+
+const dateDesc = 'dateDesc'
+const dateAsc = 'dateAsc'
+const viewDesc = 'viewDesc'
+const viewAsc = 'viewAsc'
+const likeDesc = 'likeDesc'
+const likeAsc = 'likeAsc'
+const commentDesc = 'commentDesc'
+const commentAsc = 'commentAsc'
+
+
+export default function Youtube() {
+
+//1ページあたりの動画数
+  const pageRecord = 10
+
+
+//JSONファイルの取得
+  const [videoData, setVideoData] = useState<items[]>([])
+
+  useEffect(() =>{
+    (async() => {
+      try{
+        const fetchData = await getYoutube()
+        setVideoData(fetchData)
+        setPageData(fetchData.slice(0, pageRecord))
+        console.log(fetchData)
+      } catch (error) {
+        console.error("Error Fetching Data:", error)
+      }
+    })()
+  }, [])
+
+  console.log("client:" + new Date())
+
+
+//ソート
+  const [sort, setSort] = useState('')
+
+  const sortHandleChange = (event: SelectChangeEvent) => {
+    setSort(event.target.value as string)
+
+    switch (event.target.value) {
+
+      case dateDesc:
+        videoData.sort(
+          (a, b) => new Date(b.snippet.publishedAt) - new Date(a.snippet.publishedAt)
+        )
+        break
+
+      case dateAsc:
+        videoData.sort(
+          (a, b) => new Date(a.snippet.publishedAt) - new Date(b.snippet.publishedAt)
+        )
+        break
+
+      case viewDesc:
+        videoData.sort(
+          (a, b) => parseInt(b.statistics.viewCount) - parseInt(a.statistics.viewCount)
+        )
+        break
+
+      case viewAsc:
+        videoData.sort(
+          (a, b) => parseInt(a.statistics.viewCount) - parseInt(b.statistics.viewCount)
+        )
+        break
+
+      case likeDesc:
+        videoData.sort(
+          (a, b) => parseInt(b.statistics.likeCount) - parseInt(a.statistics.likeCount)
+        )
+        break
+
+      case likeAsc:
+        videoData.sort(
+          (a, b) => parseInt(a.statistics.likeCount) - parseInt(b.statistics.likeCount)
+        )
+        break
+
+      case commentDesc:
+        videoData.sort(
+          (a, b) => parseInt(b.statistics.commentCount) - parseInt(a.statistics.commentCount)
+        )
+        break
+
+      case commentAsc:
+        videoData.sort(
+          (a, b) => parseInt(a.statistics.commentCount) - parseInt(b.statistics.commentCount)
+        )
+        break
+
+    }
+
+    setPageData(videoData.slice(0, pageRecord))
+
+  }
+
+
+//ページネーション
+  const [page, setPage] = useState<number>(1)
+
+  const [pageData, setPageData] = useState<items[]>([])
+
+  const pageHandleChange = (event: any, value: number) => {
+    setPage(value)
+
+    const pageStart = (value - 1) * pageRecord
+
+    const pageEnd = pageStart + pageRecord
+
+    setPageData(videoData.slice(pageStart, pageEnd))
+
+    window.scrollTo(0, 0)
+  }
+
+
+  const pageAll = Math.floor(videoData.length / pageRecord) + 1
+
+
+//ページ描画
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+
+      <header>
+        <div className='h-28 bg-red-600 flex items-center justify-center'>
+          <div className='sm:w-8/12 w-10/12'>
+            <h1 className='sm:text-4xl text-3xl text-white'>けんぴ。ちゃんテナ</h1>
+            <p className='pt-2 pl-2 text-gray-800'>from Youtube</p>
+          </div>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <main className="flex min-h-screen flex-col items-center justify-center py-10 bg-gradient-to-br from-white via-blue-100 to-red-100">
+        
+        <div className='sm:w-8/12 w-10/12'>
+          <h2 className='pb-8 pl-2 text-lg'>@けんぴ。ちゃんねる</h2>
+          <div className='pb-8 pl-2 flex items-center justify-left'>
+            <Box sx={{ minWidth: 200 }}>
+              <FormControl fullWidth>
+                <InputLabel id="sort-pulldown-label">並び替え</InputLabel>
+                <Select
+                  labelId="sort-pulldown-label"
+                  id="sort-pulldown"
+                  value={sort}
+                  label="並び替え"
+                  onChange={sortHandleChange}
+                >
+                  <MenuItem value={dateDesc}>Date(降順)</MenuItem>
+                  <MenuItem value={dateAsc}>Date(昇順)</MenuItem>
+                  <MenuItem value={viewDesc}>View(降順)</MenuItem>
+                  <MenuItem value={viewAsc}>View(昇順)</MenuItem>
+                  <MenuItem value={likeDesc}>Like(降順)</MenuItem>
+                  <MenuItem value={likeAsc}>Like(昇順)</MenuItem>
+                  <MenuItem value={commentDesc}>Comment(降順)</MenuItem>
+                  <MenuItem value={commentAsc}>Comment(昇順)</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+        <Stack spacing={2}>
+          <Pagination count={pageAll} variant="outlined" color="secondary" page={page} onChange={pageHandleChange} />
+        </Stack>
+
+
+        <div className='flex flex-col items-center justify-center'>
+          {pageData.map((video) => (
+
+            <div key={video.id} className='grid sm:grid-cols-2 sm:w-8/12 w-10/12 my-8 p-4 border-solid border-gray-600 border-2 rounded'>
+
+              <div className='flex items-center justify-center'>
+                  <a 
+                  href={`https://m.youtube.com/watch?v=${video.id}`} 
+                  target="_blank" 
+                  rel="noopener noreferrer">
+                    <Image
+                    src={video.snippet.thumbnails.high.url} 
+                    alt="サムネイル"
+                    width={360}
+                    height={270}
+                    />
+                  </a>
+              </div>
+
+              <div className='my-4 mx-4'>
+                <p className='text-sm text-blue-400'>{video.snippet.publishedAt.replace(/[T]|[Z]/g,' ')}</p>
+                <a className='sm:text-lg text-gray-600' 
+                href={`https://m.youtube.com/watch?v=${video.id}`} 
+                target="_blank" rel="noopener noreferrer">
+                  {video.snippet.title}
+                </a>
+
+                <ul className='flex pt-2 text-sm text-blue-400'>
+                  <li className='pr-2'>
+                    <span>View:&nbsp;{video.statistics.viewCount}</span>
+                  </li>
+                  <li className='pr-2'>
+                    <span>Like:&nbsp;{video.statistics.likeCount}</span>
+                  </li>
+                  <li>
+                    <span>Comment:&nbsp;{video.statistics.commentCount}</span>
+                  </li>
+                </ul>
+
+              </div>
+
+            </div>
+
+          ))}
+        </div>
+
+        <Stack spacing={2}>
+          <Pagination count={pageAll} variant="outlined" color="secondary" page={page} onChange={pageHandleChange} />
+        </Stack>
+
+      </main>
+          
+    </>
   )
 }
+
