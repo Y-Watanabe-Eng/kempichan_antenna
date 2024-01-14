c3e977400aa7470f3041f403149b1e840a50efe8
Y.Watanabe <tofu.mental@gmail.com>
2024-01-15 01:23:02 +0900

    up

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,15 +1,33 @@
+"use client"
+
+import './globals.css'
+import { useEffect, useState } from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
+
 export default function Loading() {
+
+  const [showSpinner, setShowSpinner] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSpinner(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+    <div id='spiner' className='w-full h-screen flex-col flex items-center justify-center'>
       <Box sx={{ display: 'flex' }}>
         <CircularProgress />
       </Box>
       <div className='pt-4'>
-        <span>Loading...</span>
+        <span className='animate-pulse'>Loading...</span>
       </div>
+    </div>
     </>
   );
 }
