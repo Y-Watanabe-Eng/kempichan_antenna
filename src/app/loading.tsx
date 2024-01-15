c61a44107bd6c69a384e81a4403c74bd77ae6941
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2024-01-15 20:47:00 +0900

    loading.tsx の更新

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,17 +1,12 @@
 "use client"
 
 import './globals.css'
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
 
 
 export default function Loading() {
 
   return (
     <div id='spiner' className='w-full h-screen flex-col flex items-center justify-center'>
-      <Box sx={{ display: 'flex' }}>
-        <CircularProgress />
-      </Box>
       <div className='pt-4'>
         <span className='animate-pulse'>Loading...</span>
       </div>
